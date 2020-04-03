f73929cb37655739c934f9bc23fdf5b0ee6ca661
rysch1 <rysch1@student.monash.edu>
2020-04-03 18:59:36 +1100

    Implemented MobileNetV2

--- CPC_basicModel.py
+++ CPC_basicModel.py
@@ -11,65 +11,77 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 
-class CNN(nn.Module):
+class MobileNetV2(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=0),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=0),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=0),
-            nn.BatchNorm1d(128),
-            nn.ReLU()
-        )
+        # Get Mobile Net
+        self.model = models.mobilenet_v2()
 
-    def forward(self, x):
-        return self.conv(x)
+        # Modify for one channel input, originally:
+        # nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, bias=False)
+        self.model.features[0][0] = nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1, bias=False)
+
+        # Reshape the classifier linear layer for number of outputs, originally:
+        # nn.Linear(in_features=1280, out_features=1000, bias=True)
+        self.model.classifier[1] = nn.Linear(in_features=1280, out_features=2, bias=True)
+
+    def forward(self, x):   
+        x = self.model.features(x).mean([2, 3])
+        return x
 
 
 class CDC(nn.Module):
-    def __init__(self, batch_size, seq_len):
+
+    def __init__(self, batch_size, pred_steps):
         super().__init__()
 
         self.batch_size = batch_size
-        self.seq_len = seq_len
+        self.pred_steps = pred_steps
         self.hidden_size = 256
-        self.pred_size = 128
+        self.pred_size = 1280
 
-        # Define Encoder Network
-        self.enc = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=0),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=0),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Conv2d(64, self.pred_size, kernel_size=5, stride=1, padding=0),
-            nn.BatchNorm1d(self.pred_size),
-            nn.ReLU()
-        )
+        # Define Encoder Network (Reshaped I/O MobileNetV2)
+        self.enc = models.mobilenet_v2()
+        # Modify for one channel input
+        self.enc.features[0][0] = nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1, bias=False)
+        # Delete classifier
+        # Requires forward function to be called as: self.enc.features(x).mean([2, 3])
+        # Outputs a 1280D Vector
+        del self.enc.classifier
 
         # Define Autoregressive Network
         self.ar = nn.GRU(self.pred_size, self.hidden_size, num_layers=1, bidirectional=False, batch_first=True)
 
         # Define Predictive Networks
-        self.Wk  = nn.ModuleList([nn.Linear(self.hidden_size, self.pred_size) for i in range(seq_len)])
-
-    def forward(self, x, hidden):
-        # Encode each patch
-        # x = batch_size * 7 * 7 * 64 * 64
-        z = self.enc(x)
-
-        return z
+        self.Wk  = nn.ModuleList([nn.Linear(self.hidden_size, self.pred_size) for i in range(pred_steps)])
 
     def init_hidden(self):
-        #return torch.zeros(2*1, batch_size, 40).cuda()
-        return torch.zeros(1, self.batchsize, self.hidden_size)
+        return torch.zeros(1, 1, self.hidden_size)
+
+    def forward(self, x, h):
+        # x = 49 * 1 * 64 * 64
+        z = self.enc.features(x).mean([2, 3]).view(-1,1,1280) # z = 49 * 1280
+        
+        # Extract encoding of one patch
+        zt = z[0].view(1,1,1280)
+        c, h = self.ar(zt, h)
+
+        # TO DO:
+        # Loop through "past sequence" to create context vector
+
+        # Predict required steps    
+
+        return z, c, h
+
 
 if __name__ == "__main__":
+    net = CDC(1,0)
+    
+    x = torch.randn(49,1,64,64)
+    h = net.init_hidden()
+
+    z,c,h = net(x, h)
+    print(h.shape)
     pass
 
