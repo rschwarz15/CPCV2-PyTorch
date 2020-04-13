b8837b4eb1ce2e0caa1665a845d2579b230925aa
rschwarz15 <r.schwarz2015@gmail.com>
2020-04-13 12:32:11 +1000

    Extracted info_nce_loss to function

--- models.py
+++ models.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 import ctypes
 ctypes.cdll.LoadLibrary('caffe2_nvrtc.dll')
 
+
 class MobileNetV2(nn.Module):
     def __init__(self):
         super().__init__()
@@ -34,6 +35,7 @@ class MobileNetV2(nn.Module):
     def forward(self, x):   
         x = self.model.features(x).mean([2, 3])
         return x
+
 
 class CDC(nn.Module):
 
@@ -76,16 +78,14 @@ class CDC(nn.Module):
         # For each image traverse each column to find context vector for first 6 rows
         contexts = torch.tensor([]).to(self.device)
 
-        # for each image in batch
         for img in range(self.batch_size):
             img_contexts = torch.tensor([]).to(self.device)
-            # for each column
+
             for col in range(7):
                 # reset hidden vector at start of each column
                 h = hidden
                 col_contexts = torch.tensor([]).to(self.device)
 
-                # for each row
                 for row in range(6):
                     c, h = self.ar(encodings[img][row][col].view(1,1,1280), h) # c = 1 * 1 * 256
                     col_contexts = torch.cat([col_contexts, c.view(1,1,1,256)], 0) # col_contexts = 6 * 1 * 1 * 256
@@ -95,9 +95,7 @@ class CDC(nn.Module):
             contexts = torch.cat([contexts, img_contexts.view(1,6,7,1,256)], 0) # contexts = batch_size * 6 * 7 * 1 * 256
 
         ### FIND ALL PREDICTED VECTORS AND DETERMINE LOSS
-        total_loss = 0
-        number_preds = 0
-        number_correct = 0
+        total_loss, number_preds, number_correct = 0, 0, 0
 
         # For each prediction find the loss
         for img in range(self.batch_size):
@@ -105,45 +103,54 @@ class CDC(nn.Module):
                 for row in range(6-step):
                     for col in range(7):
                         c = contexts[img][row][col]
-                        p = self.Wk[step](c)
-                        p = F.hardtanh(p)[0]
+                        pred = self.Wk[step](c)
+                        pred = F.hardtanh(pred)[0]
                         
-                        target_encoding = encodings[img][row][col][0]
-
-                        # Calculate dot products with target_encoding and other_encodings
-                        dots = torch.tensor([]).to(self.device)
-                        predicted_dot = torch.dot(p, target_encoding).view(1)
-                        dots = torch.cat([dots, predicted_dot], 0)
-
-                        # Get set_size - 1 encodings from other images
-                        n = 0
-                        while n < self.set_size - 1:
-                            other_img = np.random.randint(self.batch_size)
-
-                            # Don't get encodings from the same image
-                            # Might change this to allow different encodings from same image?
-                            if other_img == img:
-                                continue
-                            
-                            other_encoding = encodings[img][np.random.randint(7)][np.random.randint(7)][0]
-                            other_dot = torch.dot(p, other_encoding).view(1)
-                            dots = torch.cat([dots, other_dot], 0)
-
-                            n += 1
+                        loss, correct = self.info_nce_loss(pred, encodings, img, row, col)
                         
-                        # Calculate loss
-                        cross_entropy_loss = -1 * F.log_softmax(dots, dim=0)[0]        
-                        total_loss += cross_entropy_loss
-
-                        # Calculate accuracy
-                        if torch.argmax(dots) == 0:
-                            number_correct += 1
-
+                        total_loss += loss
+                        number_correct += correct
                         number_preds += 1
 
         loss_per_pred = total_loss / number_preds
         acc = number_correct / number_preds
         return loss_per_pred, acc
+
+    def info_nce_loss(self, pred, encodings, img, row, col):
+        target_encoding = encodings[img][row][col][0]
+
+        # Calculate dot products with target_encoding and other_encodings
+        dots = torch.tensor([]).to(self.device)
+        predicted_dot = torch.dot(pred, target_encoding).view(1)
+        dots = torch.cat([dots, predicted_dot], 0)
+
+        # Get set_size - 1 other_encodings from other images
+        n = 0
+        while n < self.set_size - 1:
+            other_img = np.random.randint(self.batch_size)
+
+            # Don't get encodings from the same image
+            # Might change this to allow different encodings from same image?
+            if other_img == img:
+                continue
+            
+            other_encoding = encodings[img][np.random.randint(7)][np.random.randint(7)][0]
+            other_dot = torch.dot(pred, other_encoding).view(1)
+            dots = torch.cat([dots, other_dot], 0)
+
+            n += 1
+        
+        # Calculate loss
+        cross_entropy_loss = -1 * F.log_softmax(dots, dim=0)[0]        
+
+        # Calculate correctness
+        if torch.argmax(dots) == 0:
+            correct = 1
+        else:
+            correct = 0
+
+        return cross_entropy_loss, correct
+
 
 class CDC_encoder(nn.Module):
     def __init__(self, classifier = False):
@@ -169,6 +176,7 @@ class CDC_encoder(nn.Module):
         # Otherwise just act as an encoder
         else:
             return self.enc.features(x).mean([2, 3])
+   
 
 if __name__ == "__main__":
     net = CDC_encoder()
