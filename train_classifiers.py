1d20aa16c53a261999ed72874b6f1f4defcb9569
rschwarz15 <r.schwarz2015@gmail.com>
2020-04-17 19:11:02 +1000

    Updated test function to deal with large sizes

--- train_classifiers.py
+++ train_classifiers.py
@@ -8,17 +8,18 @@ from tqdm import tqdm
 from models import CPC_encoder, MobileNetV2
 from data_handlers import PetImagesCPCHandler, PetImagesNormalHandler
 
-# Run batch test, return accuracy and loss
+# Process a batch, return accuracy and loss
 def fwd_pass(X, y, train=False):
+    # Run the network
     if train:
-        net.train()
         net.zero_grad()
+        net.train()
+        outputs = net(X)
 
     if not train:
         net.eval()
-
-    # Run the network on a batch
-    outputs = net(X)
+        with torch.no_grad():
+            outputs = net(X)
 
     # Compute accuracy
     matches  = [torch.argmax(i) == torch.argmax(j) for i, j in zip(outputs,y)]
@@ -35,24 +36,51 @@ def fwd_pass(X, y, train=False):
 
 
 # Train net
-def train(data_handler, epochs, batch_size):
+def train(data_handler, epochs, test_size):
     for epoch in range(epochs):
 
         for batch_img, batch_lbl in tqdm(data_handler):
-            acc, loss = fwd_pass(batch_img.to(device), batch_lbl.to(device), train=True)    
+            loss, acc = fwd_pass(batch_img.to(device), batch_lbl.to(device), train=True)    
 
-        val_acc, val_loss = test(size=32)
+        val_loss, val_acc = test(size=test_size)
 
         print(f"Epoch: {epoch}\n"
-                f"Train: {round(float(acc),2)}, {round(float(loss), 4)}\n"
-                f"Test:  {round(float(val_acc),2)}, {round(float(val_loss),4)}")
+               f"Train: {round(float(loss),4)}, {round(float(acc), 4)}\n"
+                f"Test:  {round(float(val_loss),4)}, {round(float(val_acc),4)}")
 
 
+# Process test data to find validation loss/accuracy
 def test(size):
-        test_img, test_lbl = data_handler.test_batch(size=size)
-        val_acc, val_loss = fwd_pass(test_img.to(device,), test_lbl.to(device))
+    # Need to parse in small batches in order to not incur memory error
+    test_img, test_lbl = data_handler.test_batch(size=size)
+    batch_size = 20
+    total_val_acc = 0
+    total_val_loss = 0
 
-        return val_acc, val_loss
+    # In batches process the test data
+    for i in range(size // batch_size):
+        batch_test_img = test_img[batch_size*i:batch_size*(i+1)].to(device)
+        batch_test_lbl = test_lbl[batch_size*i:batch_size*(i+1)].to(device)
+        val_acc, val_loss = fwd_pass(batch_test_img, batch_test_lbl)
+        total_val_acc += val_acc * batch_size
+        total_val_loss += val_loss * batch_size
+
+        del batch_test_img, batch_test_lbl
+    
+    # Deal with leftover test samples
+    leftover = size % batch_size
+
+    if leftover != 0:
+        batch_test_img = test_img[batch_size*(i+1):].to(device)
+        batch_test_lbl = test_lbl[batch_size*(i+1):].to(device)
+        val_acc, val_loss = fwd_pass(batch_test_img, batch_test_lbl)
+        total_val_acc += val_acc * leftover
+        total_val_loss += val_loss * leftover
+
+        del batch_test_img, batch_test_lbl
+
+
+    return total_val_acc / size, total_val_loss / size
 
 
 if __name__ == "__main__":
@@ -60,52 +88,58 @@ if __name__ == "__main__":
     IMG_SIZE = 256
     device = torch.device("cuda:0")
 
-    train_selection = 1
-    epochs = 20
+    train_selection = 0
+    epochs = 50
+    batch_size = 35  
 
     if train_selection == 0:
         print("Training CDC Encoder")
-        batch_size = 16  
 
         # Load the network
-        net = CPC_encoder(batch_size=batch_size, classifier=True).to(device)
+        net = CPC_encoder(classifier=True).to(device)
         net.load_state_dict(torch.load(PATH + "trained_cpc_encoder", map_location=device))
 
         # Freeze encoder layers
         for name, param in net.named_parameters():
-            if "enc.classifier" in name:
+            if "enc.classifier" not in name:
                 param.requires_grad = False
+
+        # for name, param in net.named_parameters():
+        #     print(name, param.requires_grad)
         
         # Intitialise data handler, optimizer and loss_function
         data_handler = PetImagesCPCHandler(batch_size=batch_size,
                                             include_labels=True,
-                                            train_proportion=0.1,
+                                            train_proportion=0.056,
                                             test_proportion=0.05)
-        optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=0.001)
-        loss_function = nn.MSELoss()
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=5e-2)
 
-        # Train CPC encoder for classification
-        train(data_handler=data_handler, epochs=epochs, batch_size=batch_size)
     elif train_selection == 1:
         print("Training MobileNet")
-        batch_size = 32  
 
         # Load the network
         net = MobileNetV2().to(device)
 
         # Intitialise data handler, optimizer and loss_function
         data_handler = PetImagesNormalHandler(batch_size=batch_size, 
-                                                train_proportion=0.95, 
+                                                train_proportion=0.028, 
                                                 test_proportion=0.05)
-        optimizer = optim.Adam(net.parameters(), lr=0.1)
-        loss_function = nn.MSELoss()
+        optimizer = optim.Adam(net.parameters(), lr=1e-3)
 
-        # Train MobileNet
-        train(data_handler=data_handler, epochs=epochs, batch_size=batch_size)
+    # Train chosen network
+    loss_function = nn.MSELoss()
+    train(data_handler=data_handler, epochs=epochs, test_size=100)
 
+    # Print final test accuracy
+    val_loss, val_acc = test(size=1000)
+    print(f"Final Accuracy: {round(val_acc*100, 2)}%")
 
+# 1%  of ImageNet = 140  images per class (train_proportion = 0.0056)
+# 5%  of ImageNet = 700  images per class (train_proportion = 0.028)
+# 10% of ImageNet = 1400 images per class (train_proportion = 0.056)
+# 50% of ImageNet = 7000 images per class (train_proportion = 0.28)
+# 100% of ImageNet = 14000 images per class (train_proportion = 0.56)
 
-    
 
 
 
